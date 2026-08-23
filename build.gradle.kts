aad7b0ddcd066d14f387b78b46e4533b15558ecf
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 01:49:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 buildscript {
     repositories {
         google()
@@ -8,9 +5,10 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.1.1")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        // D8/R8 meta-veri çökmesini önlemek için Kotlin sürümünü sabitliyoruz:
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
@@ -19,47 +17,5 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.configure("cloudstream", configuration)
-
-fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.configure("android", configuration)
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
-    }
-
-    android {
-        compileSdkVersion(34)
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 34
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        compilerOptions {
-            freeCompilerArgs.add("-Xskip-metadata-version-check")
-        }
-    }
-
-    dependencies {
-        val compileOnly by configurations
-        compileOnly("com.github.recloudstream:cloudstream:master-SNAPSHOT")
-        // DÜZELTME: nicehttp, lagradost deposundan çağrılıyor
-        compileOnly("com.github.lagradost:nicehttp:master-SNAPSHOT")
-        compileOnly("org.jsoup:jsoup:1.17.2")
     }
 }
