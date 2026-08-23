b55fd09473bb579af6031cd6297658b09761ad75
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 01:31:42 +0300

    Update HdFilmCehennemiPlugin.kt

--- HdFilmCehennemiProvider/src/main/kotlin/com/hdfilmcehennemi/HdFilmCehennemiPlugin.kt
+++ HdFilmCehennemiProvider/src/main/kotlin/com/hdfilmcehennemi/HdFilmCehennemiPlugin.kt
@@ -5,8 +5,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class HdFilmCehennemiPlugin: Plugin() {
+class HdFilmCehennemiPlugin : Plugin() {
     override fun load(context: Context) {
+        // Sağlayıcımızı (Provider) sisteme kaydediyoruz
         registerMainAPI(HdFilmCehennemiProvider())
     }
 }
