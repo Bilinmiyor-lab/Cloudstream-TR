a6923657abf105c0c540b235b6b378b6680a2ac0
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 01:03:21 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
+import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-plugins {
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.lagradost.cloudstream3.gradle") version "master-SNAPSHOT" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        // En stabil versiyonlara sabitliyoruz
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
 }
 
 allprojects {
@@ -14,32 +23,36 @@ allprojects {
     }
 }
 
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.configure("cloudstream", configuration)
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.configure("android", configuration)
+
 subprojects {
     apply(plugin = "com.android.library")
-    apply(plugin = "org.jetbrains.kotlin.android")
+    apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    extensions.configure<CloudstreamExtension>("cloudstream") {
+    cloudstream {
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
     }
 
-    extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+    android {
         compileSdkVersion(34)
-
         defaultConfig {
-            minSdk = 21
-            targetSdk = 34
+            minSdkVersion(21)
+            targetSdkVersion(34)
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
         }
+    }
 
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions {
-                freeCompilerArgs.add("-Xskip-metadata-version-check")
-            }
-        }
+    // KRİTİK ÇÖZÜM: Bulunamayan kütüphaneleri projeye zorla entegre ediyoruz.
+    dependencies {
+        "compileOnly"("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+        "compileOnly"("org.jsoup:jsoup:1.17.2")
     }
 }
