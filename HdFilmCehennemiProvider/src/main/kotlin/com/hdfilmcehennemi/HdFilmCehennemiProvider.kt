3df2df56006b0547f8ff59e5f911a2afd1086dcd
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 01:40:01 +0300

    Update HdFilmCehennemiProvider.kt

--- HdFilmCehennemiProvider/src/main/kotlin/com/hdfilmcehennemi/HdFilmCehennemiProvider.kt
+++ HdFilmCehennemiProvider/src/main/kotlin/com/hdfilmcehennemi/HdFilmCehennemiProvider.kt
@@ -1,53 +1,36 @@
 package com.hdfilmcehennemi
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
-import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+@CloudstreamPlugin
 class HdFilmCehennemiProvider : MainAPI() {
-    override var mainUrl = "https://www.hdfilmcehennemi.nl"
-    override var name = "HDFilmcehennemi"
-    override var supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override var mainUrl = "https://hdfilmcehennemi.com"
+    override var name = "HdFilmCehennemi"
+    override val supportedTypes = setOf(TvType.Movie)
     override var lang = "tr"
     override val hasMainPage = true
 
-    override val mainPage = mainPageOf(
-        "$mainUrl/page/" to "Son Eklenenler",
-        "$mainUrl/kategori/en-cok-izlenenler/page/" to "En Çok İzlenenler",
-        "$mainUrl/kategori/imdb-7-uzeri-filmler/page/" to "IMDb 7+"
-    )
-
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("${request.data}$page").document
-        val home = document.select("div.card-body, div.poster").mapNotNull {
-            it.toSearchResult()
+        val document = app.get(mainUrl).document
+        val home = document.select("div.film-box").mapNotNull { element ->
+            val title = element.select("h3").text()
+            val href = element.select("a").attr("href")
+            val posterUrl = element.select("img").attr("data-src").ifEmpty { element.select("img").attr("src") }
+            
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = posterUrl
+            }
         }
         return newHomePageResponse(request.name, home)
     }
 
-    private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("a.title, h2, img")?.attr("alt") ?: this.selectFirst("a")?.text() ?: return null
-        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src") ?: this.selectFirst("img")?.attr("src"))
-
-        return newMovieSearchResponse(title, href, TvType.Movie) {
-            this.posterUrl = posterUrl
-        }
-    }
-
-    override suspend fun search(query: String): List<SearchResponse> {
-        val document = app.get("$mainUrl/search?q=$query").document
-        return document.select("div.poster, div.card").mapNotNull {
-            it.toSearchResult()
-        }
-    }
-
-    override suspend fun load(url: String): LoadResponse? {
+    override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
-        val title = document.selectFirst("h1")?.text() ?: return null
-        val poster = fixUrlNull(document.selectFirst("div.poster img")?.attr("src"))
-        val description = document.selectFirst("article.article-content, div.overview")?.text()
+        val title = document.select("h1").text()
+        val poster = document.select("div.poster img").attr("src")
+        val description = document.select("div.icerik-detay").text()
 
         return newMovieLoadResponse(title, url, TvType.Movie, url) {
             this.posterUrl = poster
@@ -55,19 +38,11 @@ class HdFilmCehennemiProvider : MainAPI() {
         }
     }
 
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).document
-        val iframes = document.select("iframe")
-
-        for (iframe in iframes) {
-            val src = fixUrlNull(iframe.attr("src") ?: iframe.attr("data-src")) ?: continue
-            loadExtractor(src, data, subtitleCallback, callback)
-        }
+        val videoUrl = document.select("iframe").attr("src")
+        
+        loadExtractor(videoUrl, data, subtitleCallback, callback)
         return true
     }
 }
