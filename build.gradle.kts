bbbfb855ffc98eb4e6869d0752d319a6314c41ae
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 01:52:49 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
 buildscript {
     repositories {
         google()
@@ -7,7 +10,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        // D8/R8 meta-veri çökmesini önlemek için Kotlin sürümünü sabitliyoruz:
+        // Kotlin sürümünü R8/D8 hatasını önlemek için sabitliyoruz:
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
@@ -17,5 +20,31 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+    }
+}
+
+// Alt projelerdeki blokların tanınması için gerekli fonksiyonlar:
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName("cloudstream").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Cloudstream-TR/Cloudstream-TR")
+    }
+
+    android {
+        compileSdkVersion(33)
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 33
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
     }
 }
