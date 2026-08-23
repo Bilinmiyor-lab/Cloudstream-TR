76ca97f71ed3d7e217739b6d085e9ba886e2e118
Bilinmiyor-lab <emrebirdemir@gmail.com>
2026-08-24 00:25:25 +0300

    Create build.gradle.kts

---
+++ HdFilmCehennemiProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "HDFilmcehennemi Eklentisi"
+    authors = listOf("Bilinmiyor")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.hdfilmcehennemi.nl/favicon.ico"
+    isCrossPlatform = true
+}
+
+android {
+    namespace = "com.hdfilmcehennemi"
+}
